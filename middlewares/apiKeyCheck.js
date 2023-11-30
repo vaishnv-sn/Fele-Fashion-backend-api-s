5ce7afb49f440f739755bc695ab097dfd1c145f5
vaishnv-sn <vaishnav.sn4@gmail.com>
2023-11-30 22:00:48 +0530

    api-key spelling changed

--- middlewares/apiKeyCheck.js
+++ middlewares/apiKeyCheck.js
@@ -1,7 +1,7 @@
 const checkApiHeaders = (req, res, next) => {
     const apiKey = req.headers['x-api-key'];
 
-    if (!apiKey || apiKey !== 'abcd-efgh-ijlk-1234') {
+    if (!apiKey || apiKey !== 'abcd-efgh-ijkl-1234') {
         return res.status(401).json({ message: 'Unauthorized: Invalid API key' });
     }
 
